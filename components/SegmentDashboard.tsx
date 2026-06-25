51eee156d6c1f53db6644f948cf3a3db794ecf8a
jwseoinnospc-max <jwseo.innospc@gmail.com>
2026-06-25 18:44:14 +0900

    Label segment growth pills with which segment (Launch / Space Systems)

--- components/SegmentDashboard.tsx
+++ components/SegmentDashboard.tsx
@@ -172,11 +172,11 @@ function CompareCard({
           </div>
 
           <a className="pill blue" href={sourceHref}>
-            전년 동기 대비 {pct(launchGrowth)} 증가
+            Launch 매출: 전년 동기 대비 {pct(launchGrowth)} 증가
           </a>
 
           <a className="pill gray" href={sourceHref}>
-            전년 동기 대비 {pct(systemsGrowth)} 증가
+            Space Systems 매출: 전년 동기 대비 {pct(systemsGrowth)} 증가
           </a>
         </div>
 
