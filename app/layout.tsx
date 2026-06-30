c931ffec9eb894722531eb836b2c6f1012de7e9c
jwseoinnospc-max <jwseo.innospc@gmail.com>
2026-06-30 10:43:32 +0900

    Apply Pretendard font globally via * selector

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <link
           rel="stylesheet"
-          as="style"
-          crossOrigin=""
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.css"
         />
       </head>
