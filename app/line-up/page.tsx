dbfd2e07c9477c6b56117caf0939d36d4a3704bd
jwseoinnospc-max <jwseo.innospc@gmail.com>
2026-07-20 17:31:27 +0900

    fix(lineup): let drag/scroll reach nano 3D model (pointer-events none on overlays)

--- app/line-up/page.tsx
+++ app/line-up/page.tsx
@@ -193,7 +193,8 @@ function SCPortrait({ drawFn, blendMode = "normal" }: {
   return (
     <canvas ref={ref} style={{
       position: "absolute", inset: 0, width: "100%", height: "100%",
-      display: "block", mixBlendMode: blendMode as React.CSSProperties["mixBlendMode"],
+      display: "block", pointerEvents: "none",
+      mixBlendMode: blendMode as React.CSSProperties["mixBlendMode"],
     }} />
   );
 }
